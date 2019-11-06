9f297a6e997238f25f1e6276d29fc69e2518a6d9
Evarist Ruhazwe <33553332+evaristr@users.noreply.github.com>
2019-11-06 16:09:36 -0500

    Update detector_app.py

--- obj_detect_multi/detector_app.py
+++ obj_detect_multi/detector_app.py
@@ -226,7 +226,7 @@ class ObjectDetector(object):
   def _build_graph(self):
     detection_graph = tf.Graph()
     with detection_graph.as_default():
-      od_graph_def = tf.GraphDef()
+      od_graph_def = tf.compat.v1.GraphDef()
       with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
         serialized_graph = fid.read()
         od_graph_def.ParseFromString(serialized_graph)
